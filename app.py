e91bff9709abba210cedfc0f97802c412cd9bde6
Prem Singh <priyanshusingh1307@gmail.com>
2026-08-17 13:07:09 +0530

    Data transformation added

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_transformation import DataTransformation, DataTransformationConfig
 import os,sys
 
 if __name__=="__main__":
     try:
         logging.info("Starting the ML Project")
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data,test_data=data_ingestion.initiate_data_ingestion()
+
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transormation(train_data,test_data)
     except Exception as e:
         logging.info("Exception occured")
         raise CustomException(e,sys)
